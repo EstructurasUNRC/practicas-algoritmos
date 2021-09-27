786f52b24b22b945c09aeb611f0c7f125f6012b6
sonia <spermigiani@dc.exa.unrc.edu.ar>
2021-09-26 22:26:59 -0300

    Se agregó ejemplo de implementacion de Tad en Haskell

---
+++ ejercicios-practica-3/FracPar.hs
@@ -0,0 +1,34 @@
+{-|
+Module      : FracPar
+Description : Provee una implementacion del TAD Fractional usando pares de enteros
+Copyright   : Estructuras de Datos y Alg. UNRC
+License     : GPL-3
+Ejemplo de implementacion de ADT Fractional
+usamos el estilo haddock de comentarios
+-}
+
+module FracPair where
+import Data.Ratio
+
+-- | definimos un tipo nuevo para representar los racionales
+data FracPar = Integer :/ Integer 
+
+-- | tambien implementamos el show
+instance Show FracPar where
+    show (p :/ q) = (show p) ++ "/" ++ (show q)
+
+instance Num FracPar where  -- | Implementamos la clase Num, ya que los Frac son Num 
+    (p :/ q) + (n :/ m) = ((p*m)+(n*q)) :/ (q*m) 
+    (p :/ q) * (n :/ m) = (p*n) :/ (q*m) 
+    negate (p :/ q) = (- p) :/ q
+    (p :/ q) - (n :/ m) = (p :/ q) + (negate (n :/ m))
+    abs (p :/ q) = (abs p) :/ (abs q)
+    fromInteger p = (p :/ 1) 
+    signum (p :/ q) = ((signum p) * (signum q)) :/ 1
+
+instance Fractional FracPar where -- | implementamos la division
+    (p :/ q) / (n :/ m) = (p*m) :/ (q*n)  
+    fromRational r =  (numerator r) :/ (denominator r)
+   
+
+
